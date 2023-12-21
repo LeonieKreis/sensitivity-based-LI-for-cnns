188339103f42656add81ad3fc23914fd706aae53
LeonieKreis <l.kreis@stud.uni-heidelberg.de>
2023-12-21 10:10:48 +0100

    incorporate testerror in train function

--- train_and_test_.py
+++ train_and_test_.py
@@ -1,13 +1,22 @@
 import torch
+import time
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def train(model, traindataloader, optimizer, no_epochs, scheduler, stopping_criterion):
-    model.train()
+def train(model, traindataloader,testdataloader, optimizer, no_epochs, scheduler, stopping_criterion=None, save_grad_norms=False):
+    # TODO stopping criterion
+    times = []
     losses = []
+    test_accs = []
+    grad_norms = []
+    grad_norms_layerwise = []
+    for p in model.parameters():
+        grad_norms_layerwise.append([])
+
     for e in range(no_epochs):
+        toc = time.time()
         print('\nEpoch: %d' % e)
-        
+        model.train()
         train_loss = 0
         correct = 0
         total = 0
@@ -17,6 +26,17 @@ def train(model, traindataloader, optimizer, no_epochs, scheduler, stopping_crit
             outputs = model(inputs)
             loss = torch.nn.CrossEntropyLoss(outputs, targets)
             loss.backward()
+            if save_grad_norms:
+                norm = 0
+                layer = 0
+                lr = optimizer.param_groups[0]['lr']
+                for p in model.parameters():
+                    grad_norms_layerwise[layer].append(
+                        lr*torch.square(p.grad).sum().numpy())
+                    layer += 1
+                for p in model.parameters():
+                    norm += torch.square(p.grad).sum()
+                grad_norms.append(norm)
             optimizer.step()
 
             train_loss += loss.item()
@@ -26,10 +46,15 @@ def train(model, traindataloader, optimizer, no_epochs, scheduler, stopping_crit
         losses.append(train_loss)
         acc = 100.*correct/total
         print(f'training accuracy: {acc} and loss {train_loss}') 
+        test_acc = test(model, testdataloader)
+        test_accs.append(test_acc)
+        print(f'test accuracy: {test_acc}')
+        tic=time.time()
+        times.append(tic-toc)
         scheduler.step()  
 
 
-    return losses
+    return losses, test_accs, times, grad_norms_layerwise
 
 
 def test(model,testdataloader):
